6534ca0b065c892829376585e2c21f8124a9e1a0
Todd Williams <43425186+stwilliams018@users.noreply.github.com>
2018-12-07 21:32:28 -0500

    Second Fomat Start of JS

---
+++ Assets/javascript/game.js
@@ -0,0 +1,13 @@
+function initialValues(){
+    document.getElementById("Emerald").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Diamond").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Ruby").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Yellow").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Random").innerHTML = Math.floor(Math.random() * 50);
+    document.getElementById("Score").innerHTML = 0;
+    document.getElementById("Wins").innerHTML = 0;
+    document.getElementById("Loses").innerHTML = 0;
+    console.log(id="#Emerald")
+}
+
+
