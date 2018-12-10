dbd9660689abbb828b63c5b80a92ff7f4afab994
Todd Williams <43425186+stwilliams018@users.noreply.github.com>
2018-12-09 20:27:50 -0500

    Final Submission

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,13 +1,19 @@
 function initialValues(){
-    document.getElementById("Emerald").innerHTML = Math.floor(Math.random() * 10);
-    document.getElementById("Diamond").innerHTML = Math.floor(Math.random() * 10);
-    document.getElementById("Ruby").innerHTML = Math.floor(Math.random() * 10);
-    document.getElementById("Yellow").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Eme").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Dia").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Rub").innerHTML = Math.floor(Math.random() * 10);
+    document.getElementById("Yel").innerHTML = Math.floor(Math.random() * 10);
     document.getElementById("Random").innerHTML = Math.floor(Math.random() * 50);
     document.getElementById("Score").innerHTML = 0;
     document.getElementById("Wins").innerHTML = 0;
     document.getElementById("Loses").innerHTML = 0;
-    console.log(id="#Emerald")
+    var myEme = $("Yel");
+    myEme.hide();
 }
+
+
+
+
+
 
 
